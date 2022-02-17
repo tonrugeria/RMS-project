736b74a808010282f61fb96dbefb2bf5f0fef30c
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-02-18 00:09:30 +0800

    Add eslint formatter

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-})
+        username: {
+                type: String,
+                required: true,
+        },
+        password: {
+                type: String,
+                required: true,
+        },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 module.exports = User;
