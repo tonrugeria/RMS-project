eb2c8eeb9f57adc9435e1de8591975779b081f3e
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-03-09 11:42:09 +0800

    Add additional functionalities for update mod

--- routes/routes.js
+++ routes/routes.js
@@ -77,6 +77,7 @@ router.post('/register', checkNotAuthenticated, async (req, res) => {
 function uniqueId(jobIdColumn) {
         const len = jobIdColumn.length;
         if (jobIdColumn != 0) {
+                console.log(len);
                 return jobIdColumn[len - 1].job_id + 1;
         }
         return 1;
@@ -84,13 +85,13 @@ function uniqueId(jobIdColumn) {
 
 // job-requirement get route
 router.get('/job-requirement', async (req, res) => {
-        const skill = await knex('admin.skill');
+        const adminSkill = await knex('admin.skill');
         const dept = await knex('admin.department');
         const jobType = await knex('admin.job_type');
         const job = await knex('jobs.job_opening');
         const hrRemarks = await knex('admin.remarks');
         const unique = uniqueId(job);
-        res.render('jobRequirement', { skill, dept, jobType, job, unique, hrRemarks });
+        res.render('jobRequirement', { adminSkill, dept, jobType, job, unique, hrRemarks });
 });
 
 // job-requirement post route
@@ -142,13 +143,14 @@ router.post('/job-requirement', async (req, res) => {
 // job-requirement update get route
 router.get('/job-requirement/:job_id', async (req, res) => {
         const jobId = req.params.job_id;
-        const skill = await knex('admin.skill').select();
+        const adminSkill = await knex('admin.skill');
         const dept = await knex('admin.department');
         const jobType = await knex('admin.job_type');
         const hrRemarks = await knex('admin.remarks');
+        const jobSkill = await knex('jobs.skill').where('job_id', jobId);
         const job = await knex('jobs.job_opening').where('job_id', jobId);
         const unique = jobId;
-        res.render('editJobRequirement', { skill, dept, jobType, job, unique, jobId, hrRemarks });
+        res.render('editJobRequirement', { adminSkill, dept, jobType, job, unique, jobId, hrRemarks, jobSkill });
 });
 
 // job-requirement update post route
@@ -168,6 +170,42 @@ router.post('/job-requirement/:job_id', async (req, res) => {
                 skill_level_1,
                 skill_id_2,
                 skill_level_2,
+                skill_id_3,
+                skill_level_3,
+                skill_id_4,
+                skill_level_4,
+                skill_id_5,
+                skill_level_5,
+                skill_id_6,
+                skill_level_6,
+                skill_id_7,
+                skill_level_7,
+                skill_id_8,
+                skill_level_8,
+                skill_id_9,
+                skill_level_9,
+                skill_id_10,
+                skill_level_10,
+                skill_id_11,
+                skill_level_11,
+                skill_id_12,
+                skill_level_12,
+                skill_id_13,
+                skill_level_13,
+                skill_id_14,
+                skill_level_14,
+                skill_id_15,
+                skill_level_15,
+                skill_id_16,
+                skill_level_16,
+                skill_id_17,
+                skill_level_17,
+                skill_id_18,
+                skill_level_18,
+                skill_id_19,
+                skill_level_19,
+                skill_id_20,
+                skill_level_20,
         } = req.body;
         knex('jobs.job_opening')
                 .update({
@@ -189,6 +227,42 @@ router.post('/job-requirement/:job_id', async (req, res) => {
                                         skill_level_1,
                                         skill_id_2,
                                         skill_level_2,
+                                        skill_id_3,
+                                        skill_level_3,
+                                        skill_id_4,
+                                        skill_level_4,
+                                        skill_id_5,
+                                        skill_level_5,
+                                        skill_id_6,
+                                        skill_level_6,
+                                        skill_id_7,
+                                        skill_level_7,
+                                        skill_id_8,
+                                        skill_level_8,
+                                        skill_id_9,
+                                        skill_level_9,
+                                        skill_id_10,
+                                        skill_level_10,
+                                        skill_id_11,
+                                        skill_level_11,
+                                        skill_id_12,
+                                        skill_level_12,
+                                        skill_id_13,
+                                        skill_level_13,
+                                        skill_id_14,
+                                        skill_level_14,
+                                        skill_id_15,
+                                        skill_level_15,
+                                        skill_id_16,
+                                        skill_level_16,
+                                        skill_id_17,
+                                        skill_level_17,
+                                        skill_id_18,
+                                        skill_level_18,
+                                        skill_id_19,
+                                        skill_level_19,
+                                        skill_id_20,
+                                        skill_level_20,
                                 })
                                 .where('job_id', jobId)
                                 .then(() => {
