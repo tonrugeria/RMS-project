ca8b0a6e3cbbe5ee3c8cb479030f93440d004840
Cheska Salvador <cheskaslvdr@gmail.com>
2022-03-12 02:37:14 -0800

    careers page

--- routes/careersMain.js
+++ routes/careersMain.js
@@ -8,30 +8,12 @@ const router = express.Router();
 
 // careers main page
 router.get("/careersmain", async (req, res) => {
-  const job_opening = await knex("jobs.job_opening").select();
-  const job_description = await knex("jobs.job_description").select();
-<<<<<<< HEAD
-  const role = await knex
-            .select('')
-            .insert({
-              skill_1,
-              skill_2,
-              skill_3,
-              skill_4,
-              skill_5,
-              skill_6,
-              skill_7,
-              skill_8,
-              skill_9,
-              skill_10
+  const job_opening = await knex("jobs.job_opening");
+  const job_description = await knex("jobs.job_description");
+  const skill = await knex("jobs.skill");
 
-         
-            .innerJoin('admin.skill.skill_id', 'jobs.job_opening.job_id');
 
-  res.render("careersmain", { job_opening, job_description, admin.skill });
-=======
-  res.render("careersmain", { job_opening, job_description });
->>>>>>> ab90bcdad8103ee5d02b5c99cc7d554427fa82e8
+  res.render("careersmain", { job_opening, job_description, skill});
 });
 
 module.exports = router;
