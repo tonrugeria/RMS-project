e6cce0bcabba857e0e99fb0283703e62b7508dbb
Cheska Salvador <cheskaslvdr@gmail.com>
2022-03-27 02:21:04 -0700

    filter active add draft archive

--- routes/careerDetails.js
+++ routes/careerDetails.js
@@ -14,11 +14,16 @@ router.get("/careers/job/:job_id", async (req, res) => {
   .where('job_id', jobId)
   const qualification = await knex('jobs.qualification')
   .where('job_id', jobId)
+  const category = await knex ('jobs.category')
+  .where('job_id', jobId)
+  const item = await knex ('jobs.item')
+  .where('job_id', jobId)
   const admin_department = await knex('admin.department');
   const jobSkill = await knex('jobs.job_opening')
         .innerJoin('jobs.skill', 'jobs.job_opening.job_id', 'jobs.skill.job_id')
         .innerJoin('admin.skill', 'jobs.skill.skill_id', 'admin.skill.skill_id');
-  res.render("careerDetails", {jobId, jobDetail, job_opening, jobOpening, responsibility, qualification, admin_department, jobSkill });
+      
+  res.render("careerDetails", {jobId, jobDetail, job_opening, jobOpening, responsibility, qualification, admin_department, jobSkill,category,item });
 });
 
 module.exports = router;
