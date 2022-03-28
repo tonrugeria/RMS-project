417af8aa20adea4545d66c05a91dcf95ec8752d7
Ton <tonrugeria@gmail.com>
2022-03-29 07:01:13 +0800

    application

--- routes/application.js
+++ routes/application.js
@@ -7,6 +7,16 @@ const {
 } = require("../middlewares/auth");
 const router = express.Router();
 
+router.get('/application', async (req, res) => {
+  const jobOpening = await knex('jobs.job_opening')
+  const adminSkill = await knex('admin.skill')
+  const applicants = await knex('job_application.applicant_details')
+  const technicalScore = await knex('job_application.technical_score')
+  res.render('application', {
+    jobOpening, adminSkill, applicants, technicalScore
+  })
+})
+
 router.get('/application/job/:job_id', async (req, res) => {
   const jobId = req.params.job_id
   const applicants = await knex('job_application.applicant_details')
@@ -16,17 +26,56 @@ router.get('/application/job/:job_id', async (req, res) => {
     .innerJoin('job_application.applicant_details', 'job_application.applicant_details.job_id', 'jobs.job_opening.job_id')
   const jobPosition = await knex('admin.job_position')
   const adminSkill = await knex('admin.skill')
+  const technicalScore = await knex('job_application.technical_score')
   const skillScore = await knex('job_application.technical_score')
     .innerJoin('job_application.applicant_details', 'job_application.applicant_details.application_id', 'job_application.technical_score.application_id')
     .where('job_id', jobId)
-    console.log("skill-score",skillScore);
   const jobSkill = await knex('jobs.job_opening')
     .innerJoin('jobs.skill', 'jobs.job_opening.job_id', 'jobs.skill.job_id')
     .innerJoin('admin.skill', 'jobs.skill.skill_id', 'admin.skill.skill_id');
   const applicantSkill = await knex('admin.skill')
     .innerJoin('job_application.applicant_rating', 'job_application.applicant_rating.skill_id', 'admin.skill.skill_id')
-  res.render('application', { jobOpening, jobPosition, jobSkill, applicants, applicantSkill, adminSkill, jobId, jobOpeningId, skillScore })
+  const applicantInfo = await knex('job_application.applicant_details')
+  .where({
+    job_id: jobId,
+  })
+
+  const {
+    date_of_birth
+  } = applicantInfo[0] || {}
+
+  const dob = moment(date_of_birth, 'MM/DD/YYYY')
+  const age = moment().diff(dob, 'years',false);
+
+  res.render('application', { applicantInfo, jobOpening, jobPosition, jobSkill, applicants, applicantSkill, adminSkill, jobId, jobOpeningId, skillScore, technicalScore, age })
 })
 
-router.get('/')
+router.get('/application/job/:job_id/applicant/:application_id', async (req, res) => {
+  const jobId = req.params.job_id
+  const appId = req.params.application_id
+  const adminSkill = await knex('admin.skill')
+  const applicants = await knex('job_application.applicant_details')
+    .where('job_id', jobId)
+  const applicantInfo = await knex('job_application.applicant_details')
+    .where({
+      job_id: jobId,
+      application_id: appId
+    })
+  const jobOpening = await knex('jobs.job_opening');
+  const technicalScore = await knex('job_application.technical_score')
+  const applicantScore = await knex('job_application.technical_score')
+    .where({application_id: appId})
+
+
+  const {
+    date_of_birth
+  } = applicantInfo[0] || {}
+
+  const dob = moment(date_of_birth, 'MM/DD/YYYY')
+  const age = moment().diff(dob, 'years',false);
+
+  res.render('applicationDetails', {jobId, appId,adminSkill, age, applicants, applicantInfo, applicantScore, jobOpening, technicalScore})
+  // res.redirect(`/application/job/${jobId}/applicant/${appId}`)
+})
+
 module.exports = router;
