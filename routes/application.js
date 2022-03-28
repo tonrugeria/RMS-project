7fa4ce10fd7f7312401ac33b86688a82fc5322a5
Ton <tonrugeria@gmail.com>
2022-03-28 08:26:12 +0800

    application page

--- routes/application.js
+++ routes/application.js
@@ -8,12 +8,16 @@ const {
 const router = express.Router();
 
 router.get('/application', async (req, res) => {
+  const applicants = await knex('job_application.applicant_details')
   const job_opening = await knex('jobs.job_opening');
   const jobPosition = await knex('admin.job_position')
   const jobSkill = await knex('jobs.job_opening')
-        .innerJoin('jobs.skill', 'jobs.job_opening.job_id', 'jobs.skill.job_id')
-        .innerJoin('admin.skill', 'jobs.skill.skill_id', 'admin.skill.skill_id');
-  res.render('application', { job_opening, jobPosition, jobSkill })
+    .innerJoin('jobs.skill', 'jobs.job_opening.job_id', 'jobs.skill.job_id')
+    .innerJoin('admin.skill', 'jobs.skill.skill_id', 'admin.skill.skill_id');
+  const applicantSkill = await knex('admin.skill')
+    .innerJoin('job_application.applicant_rating', 'job_application.applicant_rating.skill_id', 'admin.skill.skill_id')
+    console.log(applicantSkill);
+  res.render('application', { job_opening, jobPosition, jobSkill, applicants, applicantSkill })
 })
 
 module.exports = router;
