560ef4c37eb02523846363c6879effc33e41474e
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-03-29 21:48:39 +0800

    Fix users module edit-image error

--- routes/users.js
+++ routes/users.js
@@ -1,87 +1,105 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const knex = require("../dbconnection");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const knex = require('../dbconnection');
+const fs = require('fs');
 const {
   checkAuthenticated,
   checkNotAuthenticated,
-} = require("../middlewares/auth");
+  authRole,
+} = require('../middlewares/auth');
+
 const router = express.Router();
 
 // users
-router.get('/users', async (req, res) => {
-  const users = await knex('admin.users')
-  const userRole = await knex('admin.user_role')
+router.get('/users', checkAuthenticated, authRole([4, 1]), async (req, res) => {
+  const currentUserId = req.user.user_id;
+  const currentUser = await knex('admin.users');
+  const users = await knex('admin.users');
+  const userRole = await knex('admin.user_role');
   const role = await knex
-          .select('')
-          .from('admin.user_role')
-          .innerJoin('admin.users', 'admin.users.role_id', 'admin.user_role.role_id');
-          
-  res.render('users', {users, role , userRole});
-//        knex('admin.users', )
-//                 .select()
-//                 .then((results) => {
-//                         res.render('users', { users: results });
-//                         console.log('Select users',results);
-//                 });
+    .select('')
+    .from('admin.user_role')
+    .innerJoin('admin.users', 'admin.users.role_id', 'admin.user_role.role_id');
+
+  res.render('users', { users, role, userRole, currentUser, currentUserId });
 });
 
 // delete user
-router.get("/delete/:user_id", (req, res) => {
-knex("admin.users")
-.where("user_id", req.params.user_id)
-.del()
-.then((results) => {
-res.redirect("/users");
+router.get('/delete/:user_id', (req, res) => {
+  knex('admin.users')
+    .where('user_id', req.params.user_id)
+    .del()
+    .then((results) => {
+      res.redirect('/users');
+    });
 });
-});
-
 
 const multer = require('multer');
 const path = require('path');
+
 const storage = multer.diskStorage({
-destination: (req, file, cb) => {
-cb (null, './photo');  
-},
-filename: function (req, file, cb)  {
-cb(null,file.fieldname + '_' + Date.now() + '_' +file.originalname);
-},
+  destination: (req, file, cb) => {
+    cb(null, './photo');
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
 });
 const upload = multer({
-storage: storage,
+  storage,
 });
 router.post('/users', upload.single('photo'), async (req, res) => {
-  let name = req.body.user_name;
-  let password = req.body.password;
+  const name = req.body.user_name;
+  const { password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  let role = req.body.role_name;
-  let email = req.body.email;
-  let image = req.file.filename;
-  knex('admin.users', )
-          .insert({user_name: name, password: hashedPassword, photo: image, role_id: role, email: email})
-          .then((results) => {
-                  res.redirect('/users');
-          });
+  const role = req.body.role_name;
+  const { email } = req.body;
+  const image = req.file.filename;
+  knex('admin.users')
+    .insert({
+      user_name: name,
+      password: hashedPassword,
+      photo: image,
+      role_id: role,
+      email,
+    })
+    .then((results) => {
+      res.redirect('/users');
+    });
 });
 router.use(express.static('photo'));
 
-router.post("/edit/:user_id", upload.single('photo'), async (req, res) => {
-  let user_name = req.body.user_name;
-  let role = req.body.role_name;
-  let email = req.body.email;
-  let password = req.body.password;
+router.post('/edit/:user_id', upload.single('photo'), async (req, res) => {
+  const { user_name } = req.body;
+  const role = req.body.role_name;
+  const { email } = req.body;
+  const { password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  let new_image = '';
   if (req.file) {
-    let image= req.file.filename;
-} else {
-  const image= req.file.filename;
-}
-const image= req.file.filename;
-  knex("admin.users")
-  .where("user_id", req.params.user_id)
-  .update({ user_name: user_name, role_id: role, photo: image, email: email, password: hashedPassword,}) 
-  .then((results) => {
-  res.redirect("/users");
-});
+    new_image = req.file.filename;
+    try {
+      fs.unlinkSync(`./photo/${req.body.old_image}`);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    new_image = req.body.old_image;
+  }
+
+  knex('admin.users')
+    .where('user_id', req.params.user_id)
+    .update({
+      user_name,
+      role_id: role,
+      photo: new_image,
+      email,
+      password: hashedPassword,
+    })
+    .then((results) => {
+      res.redirect('/users');
+    });
 });
 router.use(express.static('photo'));
 module.exports = router;
