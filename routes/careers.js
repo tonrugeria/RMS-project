3fa9a92455ef7cf32ccf0e72d9612e93c51e2471
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-05-02 15:58:42 +0800

    Edit public page display

--- routes/careers.js
+++ routes/careers.js
@@ -7,15 +7,23 @@ const router = express.Router();
 
 // careers main page
 router.get('/careers', async (req, res) => {
-  const active_job_opening = await knex('jobs.job_opening').where('status', '0');
-  const jobOpening = await knex('jobs.job_opening');
+  const active_job_opening = await knex('jobs.job_opening')
+    .leftJoin('admin.department', 'jobs.job_opening.job_dept', 'admin.department.dept_id')
+    .orderBy('job_id')
+    .where({
+      dept_status: 'active',
+      status: '0',
+    });
   let admin_department = await knex('admin.department')
     .innerJoin(
       'jobs.job_opening',
       'admin.department.dept_id',
       'jobs.job_opening.job_dept'
     )
-    .where('status', '0');
+    .where({
+      dept_status: 'active',
+      status: '0',
+    });
 
   admin_department = admin_department.filter(
     (value, index, self) => index === self.findIndex((t) => t.dept_id === value.dept_id)
@@ -24,8 +32,13 @@ router.get('/careers', async (req, res) => {
   const jobSkill = await knex('jobs.job_opening')
     .innerJoin('jobs.skill', 'jobs.job_opening.job_id', 'jobs.skill.job_id')
     .innerJoin('admin.skill', 'jobs.skill.skill_id', 'admin.skill.skill_id');
-  const { date_opened } = active_job_opening[0] || {};
-  const date = moment(date_opened).format('DD MMMM YYYY');
+  const date = [];
+  active_job_opening.forEach((job) =>
+    date.push({
+      job_id: job.job_id,
+      date: moment(job.date_opened).format('DD MMMM YYYY'),
+    })
+  );
 
   res.render('careers', {
     active_job_opening,
@@ -33,7 +46,6 @@ router.get('/careers', async (req, res) => {
     admin_department,
     date,
     jobSkill,
-    jobOpening,
   });
 });
 
