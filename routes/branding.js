927ddb3148360118defc1193cc4031c84c54720e
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-05-14 21:48:19 +0800

    Update branding page

--- routes/branding.js
+++ routes/branding.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 const knex = require('../dbconnection');
 const {
   checkAuthenticated,
   checkNotAuthenticated,
   authRole,
 } = require('../middlewares/auth');
+const upload = require('../middlewares/upload');
 
 const router = express.Router();
 
@@ -43,36 +45,68 @@ router.get('/branding', checkAuthenticated, authRole([3, 1]), async (req, res) =
   });
 });
 
-router.post('/branding', checkAuthenticated, authRole([3, 1]), async (req, res) => {
-  let {
-    company_name,
-    company_logo,
-    login_bg,
-    Navigation_Bar,
-    Menu_Text,
-    Menu_Text_Archive,
-    Nameplate,
-    SideBar,
-    Normal_Text,
-    Active_Text,
-    Active_Background,
-    Hover_Background,
-    Dividing_Line,
-  } = req.body;
+router.post(
+  '/branding',
+  checkAuthenticated,
+  upload,
+  authRole([3, 1]),
+  async (req, res) => {
+    const {
+      company_name,
+      login_bg,
+      Navigation_Bar,
+      Menu_Text,
+      Menu_Text_Archive,
+      Nameplate,
+      SideBar,
+      Normal_Text,
+      Active_Text,
+      Active_Background,
+      Hover_Background,
+      Dividing_Line,
+    } = req.body;
 
-  const branding = await knex('admin.branding');
+    const branding = await knex('admin.branding');
 
-  if (company_logo == '') {
-    company_logo = null;
+    if (branding == 0) {
+      if (req.file !== undefined) {
+        await knex('admin.branding').insert({
+          company_name,
+          company_logo: req.file.filename,
+          login_bg: req.file.filename,
+        });
+        res.redirect('/branding');
+      } else {
+        await knex('admin.branding').insert({ company_name });
+        res.redirect('/branding');
+      }
+    } else {
+      await knex('admin.branding');
+      if (req.file !== undefined) {
+        await knex('admin.branding').update({
+          company_name,
+          company_logo: req.file.filename,
+          login_bg: req.file.filename,
+        });
+        res.redirect('/branding');
+      } else {
+        await knex('admin.branding').update({ company_name });
+        res.redirect('/branding');
+      }
+    }
+
+    // let new_image = '';
+    // if (req.file) {
+    //   new_image = req.file.filename;
+    //   try {
+    //     fs.unlinkSync(`./photo/${req.body.old_image}`);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // } else {
+    //   new_image = req.body.old_image;
+    // }
   }
-
-  if (branding == 0) {
-    await knex('admin.branding').insert({ company_name, company_logo });
-  } else {
-    await knex('admin.branding').update({ company_name, company_logo });
-  }
-
-  res.redirect('/branding');
-});
+);
 
 module.exports = router;
